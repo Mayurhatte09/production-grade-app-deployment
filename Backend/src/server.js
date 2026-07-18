9eb5ddf65fae209356cc0d2aa23c401e7f946758
Mayur Hatte <mayurhatte01@gmail.com>
2026-07-18 23:36:46 +0530

    update

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const cors = require("cors");
 const connectDB = require('./dbconnect/db');
-const userRoute = require('./routes/user.routes')
+const userRoute = require('./routes/user.routes');
 
-const DATABASE_URL = 'mongodb://localhost:27017/UserManagementDB';
+require("dotenv").config();
+
+const DATABASE_URL = process.env.DATABASE_URL;
 
 connectDB(DATABASE_URL);
 
 app.use(cors());
 app.use(express.json()); // Parses incoming requests with JSON payloads.
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
 
-app.use('/api/users',userRoute);
+app.use('/api/users', userRoute);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
